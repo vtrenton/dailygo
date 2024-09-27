eb23b3d1fda9471eee874fa3cbee53818a2b27e1
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-09-27 12:52:48 -0500

    added day 2 array sorting and modification!
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

---
+++ day2/revslice/go.mod
@@ -0,0 +1,3 @@
+module github.com/vtrenton/dailygo/day2/revslice
+
+go 1.22.5
---
+++ day2/revslice/revslice.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := []int{100, 5, 12, 55, 55, 15, 22, 12, 5, 8, 63, 22, 1, 12}
+	fmt.Println(revarray(input))
+	fmt.Println(rmdup(input))
+}
+
+func revarray(input []int) []int {
+	outslice := []int{}
+	for i := len(input) - 1; i >= 0; i-- {
+		outslice = append(outslice, input[i])
+	}
+	return outslice
+}
+
+func rmdup(input []int) []int {
+	for i, v := range input {
+		for m := i + 1; m <= len(input)-1; m++ {
+			if v == input[m] {
+				input = append(input[:m], input[m+1:]...)
+			}
+		}
+	}
+	return input
+}
