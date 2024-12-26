ccaabef3bc9e7f00e9b4ca95a86c3f09517f0586
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-12-25 21:48:24 -0600

    implemented my own slice generation - not relying on what is built into strings
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- day3/wordfreq/wordfreq.go
+++ day3/wordfreq/wordfreq.go
@@ -7,9 +7,11 @@ import (
 
 func main() {
 	instr := "Imagine a society that subjects people to conditions that make them terribly unhappy, then gives them the drugs to take away their unhappiness. Science fiction? It is already happening to some extent in our own society... Instead of removing the conditions that make people depressed, modern society gives them antidepressant drugs. In effect, antidepressants are a means of modifying an individual's internal state in such a way as to enable him to tolerate social conditions that he would otherwise find intolerable."
-	instrfmt := fmtstr(instr)
-	inslice := strings.Fields(instrfmt)
-	//fmt.Println(inslice)
+	inslice := fmtstr(instr)
+	//inslice := strings.Fields(instrfmt)
+	//for _, v := range inslice {
+	//	fmt.Println(v)
+	//}
 
 	m := make(map[string]int)
 	for _, v := range inslice {
@@ -21,16 +23,22 @@ func main() {
 }
 
 // This function lowercases and removes all special chars from string
-// in go strings are immutable
-// pass by reference would have no benefit here
-func fmtstr(s string) string {
-	var result strings.Builder
+// then creates a slice that splits on space and newlines
+func fmtstr(s string) []string {
+	var value strings.Builder
+	var result []string
+
 	lowers := strings.ToLower(s)
 	for i := 0; i < len(lowers); i++ {
 		curr := lowers[i]
-		if ('a' <= curr && curr <= 'z') || curr == ' ' {
-			result.WriteByte(curr)
+		if 'a' <= curr && curr <= 'z' {
+			value.WriteByte(curr)
+		}
+		// hmmm what if there is no \n as EOL
+		if curr == ' ' || curr == '\n' {
+			result = append(result, value.String())
+			value.Reset() // reset the value
 		}
 	}
-	return result.String()
+	return result
 }
